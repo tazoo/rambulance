e9d53a1e9b10da7ba09237ccd72d0c894e9bc135
tazoo <tazuko.fukuda@gmail.com>
2022-06-28 14:30:10 +0900

    on_loadでのrequire rambulance_appをやめる

--- lib/rambulance/railtie.rb
+++ lib/rambulance/railtie.rb
@@ -1,30 +1,30 @@
 module Rambulance
   class Railtie < Rails::Railtie
     initializer 'rambulance', after: :prepend_helpers_path do |app|
-      ActiveSupport.on_load(:action_controller) do
-        require "rambulance/exceptions_app"
+      # ActiveSupport.on_load(:action_controller) do
+      #   require "rambulance/exceptions_app"
+      # end
 
-        app.config.exceptions_app =
-          if app.config.respond_to?(:autoloader) && app.config.autoloader == :classic
-            ->(env) {
-              begin
-                ActiveSupport::Dependencies.load_missing_constant(Object, :ExceptionsApp)
-                ::ExceptionsApp.call(env)
-              rescue NameError
-                require "rambulance/exceptions_app" if !defined?(::Rambulance::ExceptionsApp)
-                ::Rambulance::ExceptionsApp.call(env)
-              end
-            }
-          else
-            ->(env) {
-              begin
-                ::ExceptionsApp.call(env)
-              rescue NameError
-                require "rambulance/exceptions_app" if !defined?(::Rambulance::ExceptionsApp)
-                ::Rambulance::ExceptionsApp.call(env)
-              end
-            }
-          end
+      app.config.exceptions_app =
+        if app.config.respond_to?(:autoloader) && app.config.autoloader == :classic
+          ->(env) {
+            begin
+              ActiveSupport::Dependencies.load_missing_constant(Object, :ExceptionsApp)
+              ::ExceptionsApp.call(env)
+            rescue NameError
+              require "rambulance/exceptions_app" if !defined?(::Rambulance::ExceptionsApp)
+              ::Rambulance::ExceptionsApp.call(env)
+            end
+          }
+        else
+          ->(env) {
+            begin
+              ::ExceptionsApp.call(env)
+            rescue NameError
+              require "rambulance/exceptions_app" if !defined?(::Rambulance::ExceptionsApp)
+              ::Rambulance::ExceptionsApp.call(env)
+            end
+          }
         end
 
       ActiveSupport.on_load(:after_initialize) do
